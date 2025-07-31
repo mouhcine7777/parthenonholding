b8dadea209d4ca05290a7f60b42e1c13598b58fe
unknown <Administrateur@pc.parthenon.local>
2025-07-31 11:16:17 +0100

    first commit8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
 import AIassistant from "./components/AIassistant";
 
-
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 
 
